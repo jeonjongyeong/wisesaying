ef00649a96f98c48e277a3e101a5e39a3ecaa8f7
jkkjkhh <jkkjkhh@gmail.com>
2022-05-24 11:12:53 +0900

    추가, 삭제 기능 추가

--- app.js
+++ app.js
@@ -23,96 +23,161 @@ app.use(cors(corsOptions));
 
 const port = 3000;
 
-app.get("/wisesayings", async (req, res) => {
+app.delete("/wisesayings/:id", async (req, res) => {
+  //const id = req.params.id;
+  const { id } = req.params;
+
   const [rows] = await pool.query(
-    "SELECT * FROM wisesaying ORDER BY RAND() LIMIT 1"
-  );
-
-  app.post("/wisesayings", async (req, res) => {});
-
-  app.patch("/wisesayings/:id", async (req, res) => {
-    //const id = req.params.id;
-    const { id } = req.params;
-
-    const [rows] = await pool.query(
-      `
+    `
       SELECT *
       FROM wisesaying
       WHERE id = ?
       `,
-      [id]
-    );
+    [id]
+  );
 
-    if (rows.length == 0) {
-      res.status(404).json({
-        msg: "not found",
-      });
-      return;
-    }
+  if (rows.length == 0) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
 
-    const { reg_date, content, author } = req.body;
+  const { reg_date, content, author } = req.body;
 
-    if (!reg_date) {
-      res.status(400).json({
-        msg: "reg_date required",
-      });
-      return;
-    }
+  if (!reg_date) {
+    res.status(400).json({
+      msg: "reg_date required",
+    });
+    return;
+  }
 
-    if (!content) {
-      res.status(400).json({
-        msg: "content required",
-      });
-      return;
-    }
+  if (!content) {
+    res.status(400).json({
+      msg: "content required",
+    });
+    return;
+  }
 
-    if (!author) {
-      res.status(400).json({
-        msg: "author required",
-      });
-      return;
-    }
+  if (!author) {
+    res.status(400).json({
+      msg: "author required",
+    });
+    return;
+  }
 
-    const [rs] = await pool.query(
-      `
+  const [rs] = await pool.query(
+    `
+      DELETE
+      FROM wisesaying
+      WHERE id = ?
+      `,
+    [id]
+  );
+
+  res.json({
+    msg: `${id}번 할일이 수정되었습니다.`,
+  });
+});
+
+app.post("/wisesayings/", async (req, res) => {
+  const { reg_date, content, author } = req.body;
+
+  const [rs] = await pool.query(
+    `
+      INSERT INTO wisesaying
+      SET reg_date = ?,
+      content = ?,
+      author = ?
+      `,
+    [reg_date, content, author]
+  );
+
+  res.json({
+    msg: `할일이 추가되었습니다.`,
+  });
+});
+
+app.patch("/wisesayings/:id", async (req, res) => {
+  //const id = req.params.id;
+  const { id } = req.params;
+
+  const [rows] = await pool.query(
+    `
+      SELECT *
+      FROM wisesaying
+      WHERE id = ?
+      `,
+    [id]
+  );
+
+  if (rows.length == 0) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
+
+  const { reg_date, content, author } = req.body;
+
+  if (!reg_date) {
+    res.status(400).json({
+      msg: "reg_date required",
+    });
+    return;
+  }
+
+  if (!content) {
+    res.status(400).json({
+      msg: "content required",
+    });
+    return;
+  }
+
+  if (!author) {
+    res.status(400).json({
+      msg: "author required",
+    });
+    return;
+  }
+
+  const [rs] = await pool.query(
+    `
       UPDATE wisesaying
       SET reg_date = ?,
       content = ?,
       author = ?
       WHERE id = ?
       `,
-      [reg_date, content, author, id]
-    );
+    [reg_date, content, author, id]
+  );
 
-    res.json({
-      msg: `${id}번 할일이 수정되었습니다.`,
-    });
+  res.json({
+    msg: `${id}번 할일이 수정되었습니다.`,
   });
+});
 
-  app.get("/wisesayings/:id", async (req, res) => {
-    //const id = req.params.id;
-    const { id } = req.params;
+app.get("/wisesayings/:id", async (req, res) => {
+  //const id = req.params.id;
+  const { id } = req.params;
 
-    const [rows] = await pool.query(
-      `
+  const [rows] = await pool.query(
+    `
       SELECT *
       FROM wisesaying
       WHERE id = ?
       `,
-      [id]
-    );
+    [id]
+  );
 
-    if (rows.length == 0) {
-      res.status(404).json({
-        msg: "not found",
-      });
-      return;
-    }
+  if (rows.length == 0) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
 
-    res.json(rows[0]);
-  });
-
-  res.json(rows);
+  res.json(rows[0]);
 });
 
 app.listen(port);
