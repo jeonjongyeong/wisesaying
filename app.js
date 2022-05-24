602a80bf5a722ebb6a9de47c4bf2520638d19f01
jkkjkhh <jkkjkhh@gmail.com>
2022-05-24 11:07:23 +0900

    데이터 조회, 단건조회, 수정기능 수정

--- app.js
+++ app.js
@@ -28,6 +28,90 @@ app.get("/wisesayings", async (req, res) => {
     "SELECT * FROM wisesaying ORDER BY RAND() LIMIT 1"
   );
 
+  app.post("/wisesayings", async (req, res) => {});
+
+  app.patch("/wisesayings/:id", async (req, res) => {
+    //const id = req.params.id;
+    const { id } = req.params;
+
+    const [rows] = await pool.query(
+      `
+      SELECT *
+      FROM wisesaying
+      WHERE id = ?
+      `,
+      [id]
+    );
+
+    if (rows.length == 0) {
+      res.status(404).json({
+        msg: "not found",
+      });
+      return;
+    }
+
+    const { reg_date, content, author } = req.body;
+
+    if (!reg_date) {
+      res.status(400).json({
+        msg: "reg_date required",
+      });
+      return;
+    }
+
+    if (!content) {
+      res.status(400).json({
+        msg: "content required",
+      });
+      return;
+    }
+
+    if (!author) {
+      res.status(400).json({
+        msg: "author required",
+      });
+      return;
+    }
+
+    const [rs] = await pool.query(
+      `
+      UPDATE wisesaying
+      SET reg_date = ?,
+      content = ?,
+      author = ?
+      WHERE id = ?
+      `,
+      [reg_date, content, author, id]
+    );
+
+    res.json({
+      msg: `${id}번 할일이 수정되었습니다.`,
+    });
+  });
+
+  app.get("/wisesayings/:id", async (req, res) => {
+    //const id = req.params.id;
+    const { id } = req.params;
+
+    const [rows] = await pool.query(
+      `
+      SELECT *
+      FROM wisesaying
+      WHERE id = ?
+      `,
+      [id]
+    );
+
+    if (rows.length == 0) {
+      res.status(404).json({
+        msg: "not found",
+      });
+      return;
+    }
+
+    res.json(rows[0]);
+  });
+
   res.json(rows);
 });
 
