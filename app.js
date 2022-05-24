8f50b35f47d0d9d42bed69c5bde8822b032f1075
jkkjkhh <jkkjkhh@gmail.com>
2022-05-24 11:03:08 +0900

    데이터 조회, 단건조회, 수정 추가

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mysql from "mysql2/promise";
+import cors from "cors";
 
 const pool = mysql.createPool({
   host: "localhost",
@@ -12,10 +13,20 @@ const pool = mysql.createPool({
 });
 
 const app = express();
+app.use(express.json());
+
+const corsOptions = {
+  origin: "https://cdpn.io",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
+
 const port = 3000;
 
 app.get("/wisesayings", async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM wisesaying");
+  const [rows] = await pool.query(
+    "SELECT * FROM wisesaying ORDER BY RAND() LIMIT 1"
+  );
 
   res.json(rows);
 });
